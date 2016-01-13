40acd8cd7ac31167c2423f098934d7ef5bb66c5a
mraerino <melodie124@gmail.com>
2016-01-13 15:03:38 +0100

    no Arduino.h

--- firmware/ATEMmin-particle.h
+++ firmware/ATEMmin-particle.h
@@ -30,7 +30,7 @@ you can keep a clear conscience: http://skaarhoj.com/about/licenses/
 #ifndef ATEMmin_h
 #define ATEMmin_h
 
-#include "Arduino.h"
+//#include "Arduino.h"
 #include "ATEMbase-particle.h"
 
 
